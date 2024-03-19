b5d038889b26485e33664720bf4aadead1a4b95b
thehrmwn <adityahh.rm@gmail.com>
2024-03-19 19:45:13 +0700

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,8 +8,12 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from src.components.data_transformation import DataTransformation
+
 from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -61,4 +65,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.iniatiate_data_tranformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.iniatiate_data_tranformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    best_model = model_trainer.initiate_model_trainer(train_arr, test_arr)
+    print(f"Best Model: {best_model[0]} \n \Score: {best_model[1]}")
