a942ba9a2c4b76bee63ecb5911f8d9f8b7c8f496
thehrmwn <adityahh.rm@gmail.com>
2024-03-19 14:56:35 +0700

    Update code, add transformation process

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -46,12 +49,16 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
+            
         except Exception as e:
             raise CustomException(e, sys)
 
 
         
 # Test the ingestion
-# if __name__ == "__main__":
-#     obj = DataIngestion()
-#     obj.initiate_data_ingestion()
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.iniatiate_data_tranformation(train_data, test_data)
